cd57099390f25e36e466ba1b2bb53fe4d20133d2
SACHIN <sachinskbirajdar@gmail.com>
2023-10-31 23:30:29 +0530

    module

--- day-3/main.tf
+++ day-3/main.tf
@@ -1,11 +1,10 @@
-
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-0fc5d935ebf8bc3bc"
+  instance_type_value = "t2.micro"
+  
 }
--- day-3/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ami_value = "ami-0fc5d935ebf8bc3bc"
-instance_type_value = "t2.micro"
