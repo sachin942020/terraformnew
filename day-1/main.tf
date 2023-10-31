cae4d05ae9df331e1a3ba72d5b2f183bf2db32e1
SACHIN <sachinskbirajdar@gmail.com>
2023-10-31 13:58:49 +0530

    create instance in multiple region

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,12 +1,27 @@
 provider "aws" {
+  alias = "us-east-1"
     region = "us-east-1"
   
 }
 
-resource "aws_instance" "saple_instance" {
+provider "aws" {
+  alias = "us-west-2"
+    region = "us-west-2"
+  
+}
+
+resource "aws_instance" "create_instance_east" {
   ami                     = "ami-0fc5d935ebf8bc3bc"
   instance_type           = "t2.micro"
   subnet_id               = "subnet-085cfc56c1ce276ac" 
   key_name                = "linuxkp" 
-   
+  provider                = "aws.us-east-1"
+}
+
+resource "aws_instance" "create_instance_west" {
+
+  ami                     = "ami-005f8adf84f8c5057"
+  instance_type           = "t2.micro"   
+  key_name                = "winkp"
+  provider                = "aws.us-west-2"
 }
